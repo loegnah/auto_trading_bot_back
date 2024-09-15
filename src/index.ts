58af0a115ec227ba546eee828fc919686b37df5d
loegnah <loegnah@gmail.com>
2024-09-15 22:16:15 +0900

    chore(elysia): Elysia Init

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
+import { logger } from "@chneau/elysia-logger";
 import dayjs from "dayjs";
 import timezone from "dayjs/plugin/timezone";
 import utc from "dayjs/plugin/utc";
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import { coinRoute } from "@/coin/coinRoute";
-import { discordRoute } from "@/discord/discord.route";
+import Elysia from "elysia";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-async function runApp() {
-  const app = new Hono();
+// async function runApp() {
+//   const app = new Hono();
 
-  app.use(logger());
+//   app.use(logger());
 
-  // await connectBybitWs();
+//   // await connectBybitWs();
 
-  app.route("/discord", discordRoute);
-  app.route("/coin", coinRoute);
+//   app.route("/discord", discordRoute);
+//   app.route("/coin", coinRoute);
 
-  return app;
-}
+//   return app;
+// }
 
-export default await runApp();
+// export default await runApp();
+
+new Elysia().use(logger()).listen(3000);
