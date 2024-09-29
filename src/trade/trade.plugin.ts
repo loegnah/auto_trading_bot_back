407ddf01bc3950fda60c1c9149c5463866836f9b
loegnah <loegnah@gmail.com>
2024-09-29 16:13:36 +0900

    refactor(): Remove unused code

--- src/trade/trade.plugin.ts
+++ src/trade/trade.plugin.ts
@@ -1,9 +1,6 @@
 import { Elysia } from "elysia";
-import { bybitPlugin } from "./bybit/bybit.plugin";
 import { TradeService } from "./trade.service";
 
-export const tradePlugin = new Elysia({ name: "trade" })
-  .decorate({
-    tradeService: new TradeService(),
-  })
-  .use(bybitPlugin);
+export const tradePlugin = new Elysia({ name: "trade" }).decorate({
+  tradeService: new TradeService(),
+});
